2e12910d8f8c2eb4709ac685eb5bb075eeb79ec3
Mufifnman <kjluecke@gmail.com>
2014-12-07 01:00:41 -0800

    Adding Bare Bones Web Listener and beginnings of server UI

---
+++ BeerBuzzBackend/BeerBuzzBackend/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BeerBuzzBackend
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+        // Web Server
+        WebServer webServer = new WebServer();
+        CancellationToken cancelToken = new CancellationToken();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.DataContext = this;
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            this.webServer.Listen(this.cancelToken);
+        }
+
+        //TODO: OnPropertyChanged
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
---
+++ BeerBuzzBackend/BeerBuzzBackend/WebServer.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BeerBuzzBackend
+{
+    public class WebServer
+    {
+        HttpListener httpListener;
+
+        public WebServer(int portNumber = 1234)
+        {
+            httpListener = new HttpListener();
+            httpListener.Prefixes.Add(@"http://*:" + portNumber + @"/");
+        }
+
+        public async Task Listen(CancellationToken cancel)
+        {
+            httpListener.Start();
+
+            // watch for cancellation
+            while(!cancel.IsCancellationRequested)
+            {
+                var result = httpListener.BeginGetContext(callback =>
+                {
+                    var listener = (HttpListener)callback.AsyncState;
+                    if(!listener.IsListening) return;
+
+                    var httpContext = listener.EndGetContext(callback);
+                    // handle the request in httpContext, some requests can take some time to complete
+
+                }, httpListener);
+
+                while(result.IsCompleted == false)
+                {
+                    if(cancel.IsCancellationRequested) break;
+                    await Task.Delay(100); // sleep and recheck
+                }
+            }
+            httpListener.Stop();
+        }
+    }
+}
